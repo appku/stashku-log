986149612f87ede22e6c1b744a4099c2741ba8a9
Christopher Eaton <chris.eaton@appendmedia.com>
2022-05-26 16:44:58 -0700

    Upgrade to stashku 1.0

--- log-middleware.js
+++ log-middleware.js
@@ -1,4 +1,4 @@
-import StashKu, { BaseStorageEngine } from '@appku/stashku';
+import StashKu, { BaseEngine } from '@appku/stashku';
 import ConsoleLogger from './console-logger.js';
 
 const SUPPORTED_SEVERITIES = ['debug', 'info', 'warn', 'error'];
@@ -54,7 +54,7 @@ const LogMiddleware = {
     /**
      * The middleware callback handler that is called from StashKu.
      * @param {StashKu} stashKu - The `StashKu` instance making the call.
-     * @param {BaseStorageEngine} engine - The currently active storage engine.
+     * @param {BaseEngine} engine - The currently active storage engine.
      * @param {String} severity - The severity of the log call, either "debug", "info", "warn", or "error".
      * @param {Array} args - The arguments containing information to log.
      */
